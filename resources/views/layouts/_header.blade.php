f13df7b054aa8927cba3109007b81fed0c4db17b
lichen <Mytczc521@outlook.com>
2018-12-24 02:46:42 +0000

    blog

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -4,7 +4,7 @@
     </div>
     <div class="author">
        <a href = "#">
-            <img src="{{asset('storage/images/blog-tx.jpg')}}" alt="头像" title = "简历" />
+            <img src="https://blog-image-1258135583.cos.ap-chengdu.myqcloud.com/blog-TX.jpg?q-sign-algorithm=sha1&q-ak=AKID83pyRau9Yb1ao2l9Rr55qVCfRAu4IrIx&q-sign-time=1545619422;1545621222&q-key-time=1545619422;1545621222&q-header-list=&q-url-param-list=&q-signature=d6b283771c4873fdf108534ba51a8ac9365f87ea&x-cos-security-token=81cd57469b6d298020ae7946fa339f38fa70f23910001" alt="头像" title = "简历" />
        </a><br/><br/>
        <a href="#" class="author-name">李晨</a>
        <div class='author-link'>
@@ -18,10 +18,10 @@
        </div>
        <div class="others">
          <a href="https://github.com/tangczc" >
-            <img src="{{asset('storage/images/github.png')}}" class='github' title="GitHub"/>
+            <img src="https://blog-image-1258135583.cos.ap-chengdu.myqcloud.com/github.png?q-sign-algorithm=sha1&q-ak=AKIDpc0U3PNNCRuOVPRuT22LpOippH73kGS7&q-sign-time=1545619507;1545621307&q-key-time=1545619507;1545621307&q-header-list=&q-url-param-list=&q-signature=b40ef90a27f0d17a5874549d09624fb2af383efc&x-cos-security-token=5e4afae0fb567f56e0b401977b71ce8311169ac010001" class='github' title="GitHub"/>
          </a>
          <a href="lee.chen1406@gmail.com">
-            <img src="{{asset('storage/images/email.png')}}" class='email' title="邮箱"/>
+            <img src="https://blog-image-1258135583.cos.ap-chengdu.myqcloud.com/email.png?q-sign-algorithm=sha1&q-ak=AKIDqZ2ZAH8C937S58AC5Q0CRjv3MWUhlOIk&q-sign-time=1545619541;1545621341&q-key-time=1545619541;1545621341&q-header-list=&q-url-param-list=&q-signature=c937ea3de498846962ca81cc1c7c2a009c456ed5&x-cos-security-token=11c8125ad0e0bdf530f5bed8dd14b6cb2901f51d10001" class='email' title="邮箱"/>
          </a>
        </div> 
     </div>
